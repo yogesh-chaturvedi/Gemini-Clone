191d0a2bb6a97899d57ee91b1b6fb632c7418b7c
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-06-11 14:41:28 +0530

    add icon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { getContext } from '../App'
 import Time from './Time'
